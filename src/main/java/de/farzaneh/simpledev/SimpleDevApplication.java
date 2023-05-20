1aa5379c465e248e0350da174830e98115ca53cd
famiri82 <f.amiri82@gmail.com>
2023-05-20 10:00:25 +0200

    first commit

---
+++ src/main/java/de/farzaneh/simpledev/SimpleDevApplication.java
@@ -0,0 +1,13 @@
+package de.farzaneh.simpledev;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimpleDevApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SimpleDevApplication.class, args);
+	}
+
+}
---
+++ src/test/java/de/farzaneh/simpledev/SimpleDevApplicationTests.java
@@ -0,0 +1,13 @@
+package de.farzaneh.simpledev;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimpleDevApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
